bcfbffeb9ec84558d94e39eaee360cfd3f594ccb
nathansso <noliver@ucsd.edu>
2025-03-14 12:19:19 -0700

    removed next button

--- advanced.js
+++ advanced.js
@@ -641,9 +641,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   d3.select("#resetBrush").on("click", resetBrush);
   
-  document.getElementById("next-button").addEventListener("click", () => {
-    window.location.href = "mouseDetail.html";
-  });
+
 });
 
 document.getElementById("back-button").addEventListener("click", () => {
